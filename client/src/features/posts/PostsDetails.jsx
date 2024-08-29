4177f4039ab7e271710b9d342f906f49cbdbf9f7
kcstarless <155853062+kcstarless@users.noreply.github.com>
2024-08-29 13:42:44 +1000

    added delete post feature and edit button

--- client/src/features/posts/PostsDetails.jsx
+++ client/src/features/posts/PostsDetails.jsx
@@ -6,6 +6,7 @@ function PostsDetails() {
     const [post, setPost] = useState({});
     const [loading, setLoading] = useState(true);
     const { id } = useParams();
+    const navigate = useNavigate();
 
     useEffect(() => {
         const fetchCurrentPost = async () => {
@@ -25,13 +26,35 @@ function PostsDetails() {
         fetchCurrentPost();
     }, [id]);
 
+    const deletePost = async () => {
+        try {
+            const response = await fetch(`${API_URL}/${id}`, {
+                method: 'DELETE',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            });
+            if (response.ok) {
+                navigate('/');
+            } else {
+                throw response;
+            }
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
     if (loading) return <div>Loading...</div>;
 
   return (
     <div>
       <h2>{post.title}</h2>
       <p>{post.body}</p>
+      <Link to={`/posts/${id}/edit`}>Edit</Link>
+        {" | "}
       <Link to="/">Back to Posts List</Link>
+        {" | "}
+      <button onClick={deletePost}>Delete</button>
     </div>
   );
 }
--- client/src/features/posts/PostsList.jsx
+++ client/src/features/posts/PostsList.jsx
@@ -30,6 +30,25 @@ function PostsList() {
         fetchPosts();
     }, []);
 
+    const deletePost = async (id) => {
+        try {
+            const response = await fetch(`${API_URL}/${id}`, {
+                method: 'DELETE',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            });
+            if (response.ok) {
+                setPosts(posts.filter((post) => post.id !== id));
+            } else {
+                throw response;
+            }
+        }
+        catch (error) {
+            console.error(error);
+        }
+    }
+
     return (
         <div>
             <h2>Posts List</h2>
@@ -38,7 +57,11 @@ function PostsList() {
                     <h3>
                         <Link to={`/posts/${post.id}`} className="post-title">{post.title}</Link> 
                     </h3>
-                    <p>{post.body}</p>
+                    <div className="post-links">
+                        <Link to={`/posts/${post.id}/edit`}>Edit</Link>
+                        {" | "}
+                        <button onClick={() => deletePost(post.id)}>Delete</button>
+                    </div>
                 </div>
             ))}
         </div>
